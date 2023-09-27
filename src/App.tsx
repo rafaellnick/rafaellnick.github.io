1fbc90dab0a4f0b9bb40e482df6adc9b80936f75
rafaellnick <rafael.nick@estudante.ufjf.br>
2023-09-27 18:05:51 -0300

    main

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import viteLogo from './assets/lauradesmonte.png'
 import './App.css'
 
 function App() {
@@ -9,24 +9,21 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        <a href="https://instagram.com/lauragismonti" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Laura Gismonti, odontologista</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          Votaram na Laura no BBB {count} vezes!
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Ado ado ado o Quintão é um viado.
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Um abraço à Central do Galo 2.0
       </p>
     </>
   )
